5eb682dc0a642ac14ec47dc65628ae5e330b28c4
maximilian-mintchev <max.mintchev@gmail.com>
2023-09-06 21:02:12 +0200

    websocket implementiert

--- src/views/MatchScreen2.js
+++ src/views/MatchScreen2.js
@@ -3,14 +3,24 @@ import { View, Text, Button, Image, StyleSheet } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 import Constants from 'expo-constants';
 import ModalComponent from './ModalComponent';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs'; // hinzugefügt
 
-
+var stompClient = null;
 export default function MatchScreen2({ navigation, route }) {
   const apiBaseUrl = Constants.expoConfig.extra.API_BASE_URL;
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [modalVisible, setModalVisible] = useState(false);
   const [reloadFlag, setReloadFlag] = useState(true); // State für den Reload-Status
+  const [privateChats, setPrivateChats] = useState(new Map());
+
+  const [userData, setUserData] = useState({
+    username: route.params.user.uuid,
+    receivername: '',
+    connected: false,
+    message: ''
+  });
 
   const fetchData = async () => {
     try {
@@ -35,6 +45,63 @@ export default function MatchScreen2({ navigation, route }) {
     }
   }, [reloadFlag]);
 
+  useEffect(() => {
+    console.log("Web Socket Init")
+    const connectToWebSocketServer = () => {
+      const sock = new SockJS(apiBaseUrl + '/ws');
+      stompClient = Stomp.over(sock);
+      stompClient.connect({}, onConnected, onError);
+    };
+
+    connectToWebSocketServer();
+  }, []);
+
+  
+  const onConnected = () => {
+    console.log("Web Socket Connected")
+    setUserData({ ...userData, "connected": true });
+    stompClient.subscribe('/chatroom/public', onMessageReceived);
+    stompClient.subscribe('/user/' + userData.username + '/private', onPrivateMessage);
+    userJoin();
+  }
+
+  const userJoin = () => {
+    var chatMessage = {
+      senderName: userData.username,
+      status: "JOIN"
+    };
+    stompClient.send("/app/message", {}, JSON.stringify(chatMessage));
+  }
+
+  const onMessageReceived = (payload) => {
+    var payloadData = JSON.parse(payload.body);
+    switch (payloadData.status) {
+      case "JOIN":
+        // if(!privateChats.get(payloadData.senderName)){
+        //     privateChats.set(payloadData.senderName,[]);
+        //     setPrivateChats(new Map(privateChats));
+        // }
+        if (payloadData.senderName !== userData.username) {
+          console.log("New User Joins");
+          // swipeRef.ser
+          // swipeRef.data = [...data, payloadData.senderName]
+          // setData([...reloadData, payloadData.senderName]);
+          // setData(prevData => [...prevData, payloadData.senderName]);
+          console.log(data);
+
+        }
+        break;
+      case "LEAVE":
+        const newData = data.filter(obj => obj !== payloadData.senderName);
+        setData(newData);
+        console.log('User Leaves');
+        // publicChats.push(payloadData);
+        // setPublicChats([...publicChats]);
+        break;
+    }
+  }
+
+
   const handleReload = () => {
     setReloadFlag(true); // Setze den Reload-Status auf true, um den Effekt auszulösen
   };
@@ -46,6 +113,61 @@ export default function MatchScreen2({ navigation, route }) {
   const closeModal = () => {
     setModalVisible(false);
   };
+
+  const onPrivateMessage = (payload) => {
+    // console.log(payload);
+    var payloadData = JSON.parse(payload.body);
+    console.log('Open Modal');
+    openModal();
+    if (privateChats.get(payloadData.senderName)) {
+      privateChats.get(payloadData.senderName).push(payloadData);
+      setPrivateChats(new Map(privateChats));
+    } else {
+      let list = [];
+      list.push(payloadData);
+      privateChats.set(payloadData.senderName, list);
+      setPrivateChats(new Map(privateChats));
+    }
+  }
+
+  const onError = (err) => {
+    console.log(err);
+
+  }
+
+
+
+  const sendPrivateValue = (receiverName, message) => {
+    if (stompClient) {
+      var chatMessage = {
+        senderName: userData.username,
+        receiverName: receiverName,
+        message: message,
+        status: "MESSAGE"
+      };
+
+
+      console.log(stompClient.status);
+      stompClient.send("/app/private-message", {}, JSON.stringify(chatMessage));
+      setUserData({ ...userData, "message": "" });
+    }
+  }
+
+  const onSwipeLeft = (cardIndex) => {
+    openModal()
+  }
+
+
+  const onSwipeRight = (cardIndex) => {
+    const matcherId = route.params.user.uuid;
+    const matchStatus = 'INTERESTED';
+    const matcheeId = data[cardIndex].uuid;
+
+    const form = new FormData();
+    form.append('matchStatus', matchStatus);
+
+    sendPrivateValue(matcheeId, matchStatus);
+  }
 
   return (
     <View style={{ flex: 1, backgroundColor: '#000' }}>
@@ -66,7 +188,7 @@ export default function MatchScreen2({ navigation, route }) {
               cardIndex={0}
               animateCardOpacity
               verticalSwipe={false}
-              onSwipedRight={openModal}
+              onSwipedRight={onSwipeRight}
               // onSwipedLeft={onSwipeLeft}
               overlayLabels={{
                 left: {
